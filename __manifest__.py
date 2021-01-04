8b49d36ac4662b68e3a26c244e4f5b1edda5affa
r34mod <48959634+r34mod@users.noreply.github.com>
2021-01-04 10:04:00 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Centros escolares',
+    'sequence': 120,
+    'version': '1.0',
+    'depends': ['base'],
+    'category': 'Human Resources',
+    'summary': 'Centros escolares',
+    'description': """
+	Módulo de prueba
+    """,
+    'data': [
+    'views/centro_view.xml',
+    
+	
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+}
